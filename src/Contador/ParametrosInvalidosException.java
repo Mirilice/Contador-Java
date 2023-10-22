ab735108a0f151b12512f46796ecfc0932101366
Mirilice <mariaalicepco10@gmail.com>
2023-10-22 13:50:03 -0300

    Classes

---
+++ src/Contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Contador;
+
+public class ParametrosInvalidosException extends Exception{
+	public ParametrosInvalidosException(){
+	      super("O segundo parâmetro deve ser maior que o primeiro");
+	    }
+}
